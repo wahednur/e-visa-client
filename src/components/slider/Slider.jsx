0736939e06500a14f073a0d363c7fcbeca07b5f1
Abdul Wahed Nur <wahednur@gmail.com>
2025-03-04 01:08:55 +0600

    3 slider done

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -2,6 +2,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import img1 from "/img/slide1.jpg";
 import img2 from "/img/slide2.png";
 import img3 from "/img/slide3.jpg";
+import "animate.css";
 import "swiper/css";
 import "swiper/css/navigation";
 import { Autoplay, Pagination, Navigation, A11y } from "swiper/modules";
@@ -10,11 +11,11 @@ const Slider = () => {
     <Swiper
       spaceBetween={30}
       centeredSlides={true}
-      // autoplay={{
-      //   delay: 2500,
-      //   disableOnInteraction: false,
-      // }}
-      // loop={true}
+      autoplay={{
+        delay: 2500,
+        disableOnInteraction: false,
+      }}
+      loop={true}
       pagination={{
         clickable: true,
       }}
@@ -24,18 +25,64 @@ const Slider = () => {
     >
       <SwiperSlide>
         <div
-          className={`bg-[url(${img1})] w-full aspect-slider bg-cover bg-no-repeat animate__animated animate__fadeInRight`}
+          className={`slider bg-[url(/img/slide1.jpg)] w-full aspect-slider bg-cover bg-no-repeat bg-top`}
         >
-          <div className="w-full lg:w-7/12 items-center h-full">
-            <h1>Best Immigration & Visa Consultation</h1>
+          <div className="container h-full">
+            <div className="w-full lg:w-7/12 flex justify-center h-full flex-col space-y-5 text-white animate__animated animate__fadeInLeft">
+              <h1 className="sec-title text-white leading-tight">
+                Best Immigration & Visa Consultation
+              </h1>
+              <p>
+                There are many variations of passages of Lorem Ipsum available,
+                but the majority have suffered services Company
+              </p>
+              <div>
+                <button className="btn uppercase">Discover more</button>
+              </div>
+            </div>
           </div>
         </div>
       </SwiperSlide>
       <SwiperSlide>
-        <img src={img2} alt="" />
+        <div
+          className={`slider-2 bg-[url(/img/slide2.png)] w-full aspect-slider bg-contain bg-no-repeat animate__animated animate__fadeInRight`}
+        >
+          <div className="container flex h-full">
+            <div className="w-5/12 hidden lg:block"></div>
+            <div className="w-full lg:w-7/12 flex justify-center h-full flex-col space-y-5 text-white animate__animated animate__fadeInLeft">
+              <h1 className="sec-title text-white leading-tight">
+                We are professional Expert in Immigration
+              </h1>
+              <p>
+                There are many variations of passages of Lorem Ipsum available,
+                but the majority have suffered services Company
+              </p>
+              <div>
+                <button className="btn uppercase">Discover more</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </SwiperSlide>
       <SwiperSlide>
-        <img src={img3} alt="" />
+        <div
+          className={`bg-[url(/img/slide3.jpg)] w-full aspect-slider bg-cover bg-no-repeat animate__animated animate__fadeInRight`}
+        >
+          <div className="container flex h-full">
+            <div className="w-full lg:w-7/12 flex justify-center h-full flex-col space-y-5 text-white animate__animated animate__fadeInLeft">
+              <h1 className="sec-title text-white leading-tight">
+                We are professional Expert in Immigration
+              </h1>
+              <p>
+                There are many variations of passages of Lorem Ipsum available,
+                but the majority have suffered services Company
+              </p>
+              <div>
+                <button className="btn uppercase">Discover more</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </SwiperSlide>
     </Swiper>
   );
